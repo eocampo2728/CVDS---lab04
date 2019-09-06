df2605e0395ff80de0788339b6604b99704cc973
unknown <2120046@labinfo.is.escuelaing.edu.co>
2019-09-06 10:07:44 -0500

    google guice

--- src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -10,8 +10,11 @@ package hangman.setup.guice;
  * @author 2106913
  */
 
+import hangman.model.BonusScore;
 import hangman.model.French;
+import hangman.model.GameScore;
 import hangman.model.Language;
+import hangman.model.Spanish;
 import hangman.model.dictionary.HangmanDictionary;
 import hangman.model.dictionary.FrenchDictionaryDataSource;
 import hangman.view.HangmanNoviolentoPanel;
@@ -22,7 +25,11 @@ public class HangmanFactoryServices extends com.google.inject.AbstractModule {
     @Override
     protected void configure() {
         /* Guice dependency injection */
-        /*bind(Interface.class).to(Concrete.class);*/
+        bind(GameScore.class).to(BonusScore.class);
+        bind(Language.class).to(French.class);
+        bind(HangmanDictionary.class).to(FrenchDictionaryDataSource.class);
+        bind(Language.class).to(French.class);
+        bind(HangmanPanel.class).to(HangmanNoviolentoPanel.class);
     }
 
 }
